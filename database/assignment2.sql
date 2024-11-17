a7a148e47e81e445fead8ab1354db1f709ee8c2a
Robyn <Robyn796@mac.com>
2024-11-16 17:00:30 -0700

    please push to branch

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,26 +1,26 @@
-INSERT INTO account
+INSERT INTO public.account
      (first_name, last_name, email, userId)
 VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
 
-UPDATE account
+UPDATE public.account
 SET account_type = 'Admin'
 WHERE email = 'tony@starkent.com';
 
-DELETE FROM account
+DELETE FROM public.account
 WHERE email = 'tony@starkent.com'; first_name = 'Tony';
  last_name = 'Stark'; userId = 'Iam1ronM@n';
 
- UPDATE inventory
+ UPDATE public.inventory
 SET description = REPLACE(description, 'small interiors', 'a huge interior')
 WHERE model = 'GM Hummer';
 
 
 SELECT i.make, i.model, c.classification_name
-FROM inventory i
+FROM public.inventory i
 INNER JOIN classification c ON i.classification_id = c.classification_id
 WHERE c.classification_name = 'Sport';
 
-UPDATE inventory
+UPDATE public.inventory
 SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
     inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
 
